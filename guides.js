49f243917dd7d9eeaae7bc7837095ca270067b0b
k-money <164196651+Yungmonney1@users.noreply.github.com>
2026-07-27 23:23:10 -0600

    Update guides.js

--- guides.js
+++ guides.js
@@ -24,7 +24,7 @@ const guides = [
   { id: "hiQ-Y4rk42E", title: "How to Make Frutiger Metro Aero Effects",        tag: "GEN" },
   { id: "grrSRj5AGCA", title: "MAKE CRT TV SCREEN LINES GLITCH EFFECT | After Affects Tutorial",        tag: "AE" },
   { id: "SLWgx9gKPik", title: "manga transitions for edits | after effects",        tag: "AE" },
-  { id: "o8rgqHm2SU8", title: "ADVANCED ACID TRIP EFFECT TUTORIAL / AFTER EFFECT TUTORIAL / LSD EFFECT / COLORFUL TUTORIAL +FREE PF",        tag: "AE" },
+  { id: "o8rgqHm2SU8", title: "ADVANCED ACID TRIP EFFECT TUTORIAL + FREE PF",        tag: "AE" },
   { id: "_5zqnTmoHsQ", title: "How To Make A Cool Melting Transition | After Effects Tutorial + FREE PRESET",        tag: "AE" },
   
 ];
